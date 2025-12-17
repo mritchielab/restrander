1c6ad870989f4ee8934c5e50e3506a3f9e6eff46
Shiyi Yin <yinshiyi2020@gmail.com>
2025-12-17 02:06:38 -0800

    use m+1 rather than relying on autoexpansion of vector

--- src/ukkonenMatch.cpp
+++ src/ukkonenMatch.cpp
@@ -86,7 +86,7 @@ containsMatch(std::string &seq, std::string &key, int searchSize, int threshold)
     int m = key.length() - 1;
     int n = std::min((int)seq.length(), searchSize) - 1;
 
-    std::vector<int> C(m, 0);
+    std::vector<int> C(m + 1, 0);
     for (int i = 0; i < m; ++i) {
         C[i] = i;
     }
